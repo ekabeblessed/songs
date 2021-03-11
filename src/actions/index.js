6261d9c86635657639d3e38a393df16963404a35
ekabe blessed <ekabeblessed@icloud.com>
2021-03-11 23:51:35 +0100

    initial git commit

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+//Action Creator
+export const selectSong = (song) => {
+  //Return a action
+  return {
+    type: 'SONG_SELECTED',
+    payload: song,
+  };
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Hello there', duration: '3:15' },
+    { title: 'All stars', duration: '5:19' },
+    { title: 'When i see you again', duration: '2:27' },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
